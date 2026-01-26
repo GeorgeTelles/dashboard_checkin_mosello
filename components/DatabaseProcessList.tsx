3e73aad9c33553d237d785b7a44d0c0f37006840
George Telles <georgesmattos@gmail.com>
2026-01-26 10:24:26 -0300

    largura colunas 4

--- components/DatabaseProcessList.tsx
+++ components/DatabaseProcessList.tsx
@@ -164,15 +164,15 @@ const DatabaseProcessList: React.FC<DatabaseProcessListProps> = ({ audiences = [
                                         <p className="mt-1">{processItem.hearingDate} <br/> {processItem.hearingTime}</p>
                                     </div>
                                     <div>
-                                        <p className="font-medium text-gray-600 text-xs uppercase tracking-tight dark:text-slate-300">Hora do<br/>Check&#8209;in</p>
+                                        <p className="font-medium text-gray-600 text-xs uppercase tracking-tight dark:text-slate-300">Hora do<br/>Checkin</p>
                                         <p className="mt-1">{processItem.confirmationTime || '-'}</p>
                                     </div>
                                     <div>
-                                        <p className="font-medium text-gray-600 text-xs uppercase tracking-tight dark:text-slate-300">Status<br/>Check&#8209;out</p>
+                                        <p className="font-medium text-gray-600 text-xs uppercase tracking-tight dark:text-slate-300">Status<br/>Checkout</p>
                                         <p className="mt-1"><StatusBadge status={processItem.checkOutStatus} /></p>
                                     </div>
                                     <div>
-                                        <p className="font-medium text-gray-600 text-xs uppercase tracking-tight dark:text-slate-300">Hora do<br/>Check&#8209;out</p>
+                                        <p className="font-medium text-gray-600 text-xs uppercase tracking-tight dark:text-slate-300">Hora do<br/>Checkout</p>
                                         <p className="mt-1">{processItem.checkOutTime || '-'}</p>
                                     </div>
                                 </div>
